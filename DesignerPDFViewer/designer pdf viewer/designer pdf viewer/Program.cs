9d2a3708829e861cc53da3b409e0437dabbba37c
Chris Carruthers <chris081888@gmail.com>
2023-03-15 16:27:11 -0400

    Adding Halloween Sale and Designer PDF Viewer solutions.

---
+++ DesignerPDFViewer/designer pdf viewer/designer pdf viewer/Program.cs
@@ -0,0 +1,59 @@
+﻿// Chris Carruthers, example solution to Designer PDF Viewer on HackerRank. 03/15/2023
+// https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        //EVERYTHING ABOVE HERE IS PROVIDED BY THE QUESTION
+        //Here is where we get to play Not alot to say here because as I kept looking at my code I realized how I could keep condensing it.
+        //Now we make a list of our heights
+        List<int> heights = new List<int>();
+        foreach (char letter in word)
+        {
+            int index = char.ToUpper(letter) - 65; // Convert the character to it's upper case. This is because the capital character 'A' is = integer value 65. 'B' 66 and so on. We can work with this. We want the index of A to be 0, so -65.   
+            heights.Add(h[index]); // we will add the height value in h at the index associated with our letter to our heights list.
+        }
+        int result = heights.Max() * (word.Length); // So now we have our height * width equation. The word length gets our width. Height is the highest number in the heights list we made.
+        Console.WriteLine(result);
+        return result;
+    }
+
+}
+// EVERYTHING UNDER HERE IS CODE FROM THE QUESTION
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+    }
+}
---
+++ Halloween Sale/Halloween Sale/Program.cs
@@ -0,0 +1,81 @@
+﻿/* Chris Carruthers
+ * 03/15/2023
+ * example solution for https://www.hackerrank.com/challenges/halloween-sale/problem*/
+
+using System.CodeDom.Compiler; // question provided code
+using System.Collections.Generic; // question provided code
+using System.Collections; // question provided code
+using System.ComponentModel; // question provided code
+using System.Diagnostics.CodeAnalysis; // question provided code
+using System.Globalization; // question provided code
+using System.IO; // question provided code
+using System.Linq; // question provided code
+using System.Reflection; // question provided code
+using System.Runtime.Serialization; // question provided code
+using System.Text.RegularExpressions; // question provided code
+using System.Text; // question provided code
+using System; // question provided code
+using System.Runtime.InteropServices;
+
+class Result
+{
+
+    /*
+     * Complete the 'howManyGames' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER p
+     *  2. INTEGER d
+     *  3. INTEGER m
+     *  4. INTEGER s
+     */
+
+    public static int howManyGames(int p, int d, int m, int s) // question provided code
+    {
+        // Return the number of games you can buy
+        /* This is where we get to play
+         * We get an input of p (starting number), d (reduction of p each iteration), m(the lowest p can go), and s which is your total money.
+         */
+        int result = 0;
+        for (int i = p; i < s; i = i-d)
+        {
+            if (i <= m)
+            {
+                s = s-m;
+                result++;
+            }
+            else
+            {
+                s = s-i;
+                result++;
+            }
+        }
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args) // question provided code
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true); // question provided code
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' '); // question provided code
+
+        int p = Convert.ToInt32(firstMultipleInput[0]); // question provided code
+
+        int d = Convert.ToInt32(firstMultipleInput[1]); // question provided code 
+
+        int m = Convert.ToInt32(firstMultipleInput[2]); // question provided code
+
+        int s = Convert.ToInt32(firstMultipleInput[3]); // question provided code
+
+        int answer = Result.howManyGames(p, d, m, s); // question provided code
+
+        textWriter.WriteLine(answer); // question provided code
+
+        textWriter.Flush(); // question provided code
+        textWriter.Close(); // question provided code
+    } 
+}
